a59225a93da4efc769319853577b33d097aa45da
kjammes <jayeshkarli@gmail.com>
2023-12-10 17:41:46 +0530

    changes to read the overriding html from local resource

--- src/script.js
+++ src/script.js
@@ -1,38 +1,16 @@
-const generateHTML = () => {
-  fetch('overlay.txt')
-    // .then(response => {
-    //   if (!response.ok) {
-    //     throw new Error(`Failed to fetch: ${response.status} ${response.statusText}`);
-    //   }
-    //   return response.text();
-    // })
-    // .then(htmlContent => {
-    //   console.log('HTML content ', htmlContent);
-    //   document.body.innerHTML = htmlContent;
-    // })
-    // .catch(error => console.error('Error fetching HTML file:', error));
-
-  document.body.innerHTML = `
-  <h2>DON'T WASTE TIME ON <span>${window.location.hostname}</span></h2>
-  <h1>Below are the websites you should be visiting... 🙂</h1>
-  <ul>
-    <li>
-      <a href="https://leetcode.com/">LeetCode</a>
-    </li>
-    <li>
-      <a href="https://hashnode.com/">HashNode</a>
-    </li>
-    <li>
-      <a href="https://www.udemy.com/">Udemy</a>
-    </li>
-    <li>
-      <a href="https://pgexercises.com/">SQL Exercises</a>
-    </li>
-  </ul>
-  `
+const generateBody = () => {
+  fetch(chrome.runtime.getURL('./overlay.html'))
+    .then(response => response.text())
+    .then(result => {
+      document.body.innerHTML = result;
+      document.getElementById("social-media-site").innerText = window.location.hostname;
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
 };
 
-const generateSTYLING = () => {
+const generateStyle = () => {
   document.head.innerHTML = `<style></style>`;
 };
 
@@ -44,6 +22,6 @@ const blockedHosts = [
 if (
   blockedHosts.includes(window.location.hostname)
 )  {
-  generateSTYLING();
-  generateHTML();
+  generateBody();
+  generateStyle();
 }
